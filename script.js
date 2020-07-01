6b90825cc1de9c8ef878014b744b8578157ac78a
Laurentiu Popa <66673081+laur80@users.noreply.github.com>
2020-07-01 05:40:09 +0000

    Update script.js
    Create another element that contains operator buttons

--- script.js
+++ script.js
@@ -4,14 +4,28 @@ myOutput.classList.add('output');
 myOutput.innerHTML = '0';
 myCalculator.appendChild(myOutput);
 const myKeys = [
-  ['1', '2', '3', '+'],
-  ['4', '5', '6', '-'],
-  ['7', '8', '9', '*'],
-  ['C', '0', '=', '/'],
+  ['1', '2', '3'],
+  ['4', '5', '6'],
+  ['7', '8', '9'],
+  ['C', '0', '='],
 ];
 const myOper = ['+', '-', '*', '/'];
 
 // add calculator keys------------------------------------------------------------------
+
+for (let i = 0; i < myOper.length; i++) {
+  let div2 = document.createElement('div');
+  for (let k = 0; k < myOper.length; k++) {
+    let btnOper = document.createElement('div');
+    btnOper.innerHTML = myOper[i][k];
+    btnOper.classList.add('btn');
+    btnOper.addEventListener('click', btnHit);
+    div2.appendChild(btnOper);
+  }
+  myCalculator.appendChild(div2);
+  // console.log(div2);
+}
+
 
 for (let y = 0; y < myKeys.length; y++) {
   let div = document.createElement('div');
