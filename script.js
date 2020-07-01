cb7c66011f68af8ac0b65cc40a2e8f82e0726801
Laurentiu Popa <66673081+laur80@users.noreply.github.com>
2020-07-01 06:48:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ for (let y = 0; y < myKeys.length; y++) {
 
 for (let i = 0; i < myOper.length; i++) {
   let div2 = document.createElement('div');
-  for (let k = 0; k < myOper.length; k++) {
+  for (let k = 0; k < myOper[i].length; k++) {
     let btnOper = document.createElement('div');
     btnOper.innerHTML = myOper[i][k];
     btnOper.classList.add('btn');
