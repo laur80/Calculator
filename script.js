9df93c69378a6a618c28a280e5befaf3d884ee89
Laurentiu Popa <66673081+laur80@users.noreply.github.com>
2020-07-01 05:44:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,18 +13,6 @@ const myOper = ['+', '-', '*', '/'];
 
 // add calculator keys------------------------------------------------------------------
 
-for (let i = 0; i < myOper.length; i++) {
-  let div2 = document.createElement('div');
-  for (let k = 0; k < myOper.length; k++) {
-    let btnOper = document.createElement('div');
-    btnOper.innerHTML = myOper[i][k];
-    btnOper.classList.add('btn');
-    btnOper.addEventListener('click', btnHit);
-    div2.appendChild(btnOper);
-  }
-  myCalculator.appendChild(div2);
-  // console.log(div2);
-}
 
 
 for (let y = 0; y < myKeys.length; y++) {
@@ -40,6 +28,20 @@ for (let y = 0; y < myKeys.length; y++) {
   }
   myCalculator.appendChild(div);
   // console.log(div);
+}
+
+
+for (let i = 0; i < myOper.length; i++) {
+  let div2 = document.createElement('div');
+  for (let k = 0; k < myOper.length; k++) {
+    let btnOper = document.createElement('div');
+    btnOper.innerHTML = myOper[i][k];
+    btnOper.classList.add('btn');
+    btnOper.addEventListener('click', btnHit);
+    div2.appendChild(btnOper);
+  }
+  myCalculator.appendChild(div2);
+  // console.log(div2);
 }
 
 // add function to the buttons-------------------------------------------------------------
