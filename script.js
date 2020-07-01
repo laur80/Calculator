e0e60525dcdbc1529c4fe60bb33036cd444b07bf
Laurentiu Popa <66673081+laur80@users.noreply.github.com>
2020-07-01 09:09:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ const myOper = ['+', '-', '*', '/'];
 
 // add calculator keys------------------------------------------------------------------
 
-
-
 for (let y = 0; y < myKeys.length; y++) {
   let div = document.createElement('div');
   div.classList.add('row');
@@ -30,47 +28,62 @@ for (let y = 0; y < myKeys.length; y++) {
   // console.log(div);
 }
 
-
 for (let i = 0; i < myOper.length; i++) {
   let div2 = document.createElement('div');
+  div2.classList.add('row2');
+  // console.log(div2);
   for (let k = 0; k < myOper[i].length; k++) {
     let btnOper = document.createElement('div');
+    // console.log(btnOper);
     btnOper.innerHTML = myOper[i][k];
+    // console.log(btnOper.innerHTML);
     btnOper.classList.add('btn');
-    btnOper.addEventListener('click', btnHit);
+    btnOper.addEventListener('click', btnOperHit);
     div2.appendChild(btnOper);
   }
   myCalculator.appendChild(div2);
-  // console.log(div2);
+  // console.log(myCalculator);
+}
+function btnOperHit() {
+  let myOp = this.innerText;
+  // console.log(myOp);
+  return myOp;
 }
 
-// add function to the buttons-------------------------------------------------------------
-
-function btnHit(e) {
+function btnHit() {
   let myValue = this.innerText;
-  let myCalc = myOutput.innerText;
-  if (myCalc === '0') {
-    myCalc = '';
-  }
-  
-  if (myValue === '=') {
-    myCalc = eval(myCalc);
-  } else {
-    let lastChar = myCalc.substring(myCalc.length - 1);
-    console.log(lastChar);
-    if (myOper.includes(myValue)) {
-      if (myOper.includes(lastChar)) {
-        myCalc = myCalc.substring(0, myCalc.length - 1);
-        
-      } else {
-        myCalc = eval(myCalc);
-      }
-    }
-    myCalc = myCalc + myValue;
-  }
-  if (myValue === 'C') {
-    myCalc = '0';
-  }
-
-  myOutput.innerText = myCalc;
+  // console.log(myValue);
+  return myValue;
 }
+
+
+// // add function to the buttons-------------------------------------------------------------
+
+// function calc() {
+//  
+//   let myCalc = myOutput.innerText;
+//   if (myCalc === '0') {
+//     myCalc = '';
+//   }
+
+//   if (btnHit() === '=') {
+//     myCalc = eval(myCalc);
+//   } else {
+//     let lastChar = myCalc.substring(myCalc.length - 1);
+//     // console.log(lastChar);
+//     if (myOper.includes(btnHit())) {
+//       if (myOper.includes(lastChar)) {
+//         myCalc = myCalc.substring(0, myCalc.length - 1);
+//       } else {
+//         myCalc = eval(myCalc);
+//       }
+//     }
+//     myCalc = myCalc + btnHit();
+//   }
+//   if (myValue === 'C') {
+//     myCalc = '0';
+//   }
+
+//   myOutput.innerText = myCalc;
+// }
+
