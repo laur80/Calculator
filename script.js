e4057c2721a70561d9432e136e95b97c05d0dc47
Laurentiu Popa <66673081+laur80@users.noreply.github.com>
2020-07-02 19:14:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const myCalculator = document.querySelector('.myCalc');
 let myOutput = document.createElement('div');
 myOutput.classList.add('output');
 myOutput.innerHTML = '0';
-myCalculator.appendChild(myOutput);
+let display = myCalculator.appendChild(myOutput);
+// console.log(display);
+
+let operator = '';
+let value = '';
+let firstNumber = '';
+let secondNumber = '';
+let myCalc = '';
+
 const myKeys = [
-  ['1', '2', '3'],
-  ['4', '5', '6'],
-  ['7', '8', '9'],
-  ['C', '0', '='],
+  ['1', '2', '3', '+'],
+  ['4', '5', '6', '-'],
+  ['7', '8', '9', '*'],
+  ['C', '0', '=', '/'],
 ];
 const myOper = ['+', '-', '*', '/'];
 
 // add calculator keys------------------------------------------------------------------
-
-
 
 for (let y = 0; y < myKeys.length; y++) {
   let div = document.createElement('div');
@@ -23,54 +29,86 @@ for (let y = 0; y < myKeys.length; y++) {
     let btn = document.createElement('div');
     btn.innerHTML = myKeys[y][x];
     btn.classList.add('btn');
-    btn.addEventListener('click', btnHit);
+    btn.addEventListener('click', colectInput);
     div.appendChild(btn);
   }
   myCalculator.appendChild(div);
   // console.log(div);
 }
 
+// add function to keys-------------------------------------------------------------
 
-for (let i = 0; i < myOper.length; i++) {
-  let div2 = document.createElement('div');
-  for (let k = 0; k < myOper[i].length; k++) {
-    let btnOper = document.createElement('div');
-    btnOper.innerHTML = myOper[i][k];
-    btnOper.classList.add('btn');
-    btnOper.addEventListener('click', btnHit);
-    div2.appendChild(btnOper);
-  }
-  myCalculator.appendChild(div2);
-  // console.log(div2);
+function colectInput(e) {
+  value = this.innerText;
+  // console.log(value);
+  evaluates(value);
 }
 
-// add function to the buttons-------------------------------------------------------------
+function evaluates(value) {
+  if (isNaN(value)) {
+    evaluatesOperator(value);
+  } else {
+    evaluatesNumber(value);
+  }
+}
 
-function btnHit(e) {
-  let myValue = this.innerText;
-  let myCalc = myOutput.innerText;
+/* operators part */
+
+function evaluatesOperator(value) {
+  // console.log(value);
   if (myCalc === '0') {
     myCalc = '';
   }
-  
-  if (myValue === '=') {
-    myCalc = eval(myCalc);
-  } else {
-    let lastChar = myCalc.substring(myCalc.length - 1);
-    console.log(lastChar);
-    if (myOper.includes(myValue)) {
-      if (myOper.includes(lastChar)) {
-        myCalc = myCalc.substring(0, myCalc.length - 1);
-        
-      } else {
-        myCalc = eval(myCalc);
-      }
-    }
-    myCalc = myCalc + myValue;
-  }
-  if (myValue === 'C') {
-    myCalc = '0';
+
+  if (value === 'C') {
+    display.innerText = '0';
+    myCalc = '';
   }
 
-  myOutput.innerText = myCalc;
+  if (value === '+' || value === '-' || value === '*' || value === '/') {
+    operator = value;
+    display.innerText = operator;
+    // console.log(previousOperator);
+    firstNumber = parseInt(myCalc);
+    myCalc = '';
+    // console.log(firstNumber);
+  }
+
+  if (value === '=') {
+    calculate(firstNumber);
+  }
+}
+
+/* Numbers part */
+
+function evaluatesNumber(value) {
+  myCalc += value;
+  // console.log(myCalc);
+  printEvaluation(myCalc);
+}
+
+function printEvaluation(myCalc) {
+  display.innerText = myCalc;
+  // console.log(display.innerText);
+}
+
+// do the Math-------------------------------------------------------
+
+function calculate(firstNumber) {
+  secondNumber = parseInt(myCalc);
+  // console.log(secondNumber);
+  if (operator === '+') {
+    myCalc = firstNumber + secondNumber;
+  }
+  if (operator === '-') {
+    myCalc = firstNumber - secondNumber;
+  }
+  if (operator === '*') {
+    myCalc = firstNumber * secondNumber;
+  }
+  if (operator === '/') {
+    myCalc = firstNumber / secondNumber;
+  }
+  display.innerText = myCalc;
+  // console.log(display.innerText);
 }
